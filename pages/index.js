afa28aa92d3cbba8b57b519a0bfe88539e3f1ef9
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2021-09-18 22:43:32 +0800

    initial implementation with lottiefiles

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+function HomePage() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <Head>
+                <title>Jhune Carlo Trogelio</title>
+            </Head>
+
+            <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+                <h1 className="text-6xl font-bold">
+                    Welcome to{' '}
+                    <a className="text-blue-600" href="https://nextjs.org">
+                        Next.js!
+                    </a>
+                </h1>
+            </main>
+        </div>
+    )
+}
+
+export default HomePage
